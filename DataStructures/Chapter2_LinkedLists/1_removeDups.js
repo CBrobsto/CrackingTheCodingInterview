d4781790119db795013ce046a511ba8ce2fe8987
Cody Brobston <cbrobston22@gmail.com>
2020-07-16 18:23:59 -0700

    Added documentation to removeDups.

--- DataStructures/Chapter2_LinkedLists/1_removeDups.js
+++ DataStructures/Chapter2_LinkedLists/1_removeDups.js
@@ -1,1 +1,12 @@
-// Remove duplicate nodes from an unsorted Linked List
+/* Javascript does not have a built-in LinkedList data 
+structure so the npm package 'linked-list' will be used. 
+https://www.npmjs.com/package/linked-list */
+const LinkedList = require('linked-list');
+
+/* 'Remove duplicate nodes from an unsorted Linked List'
+ * This implementation moves forward in the list
+ * Use a reference to the head to process the whole list
+ * Begin at any node, skipping the ones before it (inclusive) */
+void function removeDups(linkedListNode) {
+    
+}
